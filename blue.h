cd3ac2a5c5dc2fe7322719bf8b139dcf6b6db4c2
Aamon Magnusson <aamonm6@gmail.com>
2022-03-12 16:44:45 -0700

    change blue

--- blue.h
+++ blue.h
@@ -1,8 +1,8 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#000000", "#005577" },
-	[SchemeOut] = { "#444444", "#005577" },
+	[SchemeSel] = { "#000000", "#82eefd" },
+	[SchemeOut] = { "#444444", "#82eefd" },
 //	[SchemeNorm] = { "#bbbbbb", "#222222" },
 //	[SchemeSel] = { "#eeeeee", "#005577" },
 //	[SchemeOut] = { "#000000", "#00ffff" },
