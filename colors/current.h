709765c16a48c60758ff6aaa438dcd4e02722d2c
Aamon Magnusson <aamonm6@gmail.com>
2022-12-30 14:44:16 -0700

    New dracula

--- colors/current.h
+++ colors/current.h
@@ -2,7 +2,7 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#f8f8f2", "#282a36" },
-	[SchemeSel] = { "#282a36", "#bd93f9" },
+	[SchemeSel] = { "#282a36", "#50fa7b" },
 	[SchemeOut] = { "#ff5555", "#ffb86c" },
 //	[SchemeNorm] = { "#bbbbbb", "#222222" },
 //	[SchemeSel] = { "#eeeeee", "#005577" },
